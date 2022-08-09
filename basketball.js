9a75017686c0d45426a2a9730c5b3f72a79141a6
Preye Omusuku <omusukup@yahoo.com>
2022-08-09 14:18:02 +0300

    Initial commit

---
+++ basketball.js
@@ -0,0 +1,31 @@
+let score=0;
+let homeScore= document.getElementById('display')
+let guestScore=document.getElementById('showcase')
+function buttonOne() {
+score+=1;
+homeScore.textContent=score;
+}
+function buttonTwo() {
+  score+=2;
+  homeScore.textContent=score;
+  }function buttonThree() {
+    score+=3;
+    homeScore.textContent=score;
+    }
+    function buttonOnes() {
+      score+=1;
+      guestScore.textContent=score;
+      }
+      function buttonTwos() {
+        score+=2;
+        guestScore.textContent=score;
+        }
+        function buttonThrees() {
+          score+=3;
+          guestScore.textContent=score;
+          }
+          function resetButtons() {
+            score=0;
+            guestScore.textContent= score;homeScore.textContent=score;
+            }
+          
